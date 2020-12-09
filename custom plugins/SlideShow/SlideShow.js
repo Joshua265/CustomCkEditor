19f851f790d0b8c2e116a8e653d81c95c3235716
Joshua265 <63151586+Joshua265@users.noreply.github.com>
2020-12-10 00:49:49 +0100

    slideshowui import fix

--- custom plugins/SlideShow/SlideShow.js
+++ custom plugins/SlideShow/SlideShow.js
@@ -1,5 +1,5 @@
 import EditSlideShow from "./editSlideShow";
-import SlideShowUi from "./slideShowUi";
+import SlideShowUi from "./SlideShowUi";
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 
 export default class SlideShow extends Plugin {
